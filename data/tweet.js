805c5bdda2c06b8648a153b2bfedc272b920d177
jiyoon.lee <jiyoon3421@gmail.com>
2023-04-29 20:27:53 +0900

    230429 jyl mongodb적용완료

--- data/tweet.js
+++ data/tweet.js
@@ -1,39 +1,59 @@
+import MongoDb from "mongodb";
 import { getTweets } from "../database/database.js";
 
+const ObjectId = MongoDb.ObjectId;
+
 export async function getAll() {
-  return getTweets()
-    .find({})
-    .sort({ createdAt: -1 })
-    .forEach((element) => {
-      console.log(element);
-    });
+  return getTweets().find({}).sort({ createdAt: -1 }).toArray().then(mapTweets);
 }
 
 export async function getByUsername(username) {
-  return getTweets().findOne({
-    where: { ["user.username"]: username },
-  });
+  return getTweets()
+    .find({ username })
+    .sort({ createdAt: -1 })
+    .toArray()
+    .then(mapTweets);
 }
 
 export async function getById(id) {
-  return getTweets().findByPk(id);
+  return getTweets()
+    .findOne({ _id: new ObjectId(id) })
+    .then(mapOptionalTweet);
 }
 
-export async function create(text, user) {
-  return getTweets().insertOne({ text, createdAt: Date(), user });
+export async function create(text, { id: userId, name, username, url }) {
+  const tweet = {
+    text,
+    createdAt: new Date(),
+    userId,
+    name,
+    username,
+    url,
+  };
+  return getTweets()
+    .insertOne(tweet) //
+    .then((data) => mapOptionalTweet({ ...tweet, _id: data.insertedId }));
 }
 
 export async function update(id, text) {
-  return getTweets().updateOne(
-    { where: { id } }, //
-    [{ $set: { text } }] //
-  );
+  return getTweets()
+    .findOneAndUpdate(
+      { _id: new ObjectId(id) }, //
+      { $set: { text } },
+      { returnDocument: "after" }
+    )
+    .then((result) => result.value)
+    .then(mapOptionalTweet);
 }
 
 export async function remove(id) {
-  return getTweets().destroy({
-    where: {
-      id,
-    },
-  });
+  return getTweets().deleteOne({ _id: new ObjectId(id) });
 }
+
+const mapTweets = (tweets) => {
+  return tweets.map(mapOptionalTweet);
+};
+
+const mapOptionalTweet = (tweet) => {
+  return tweet ? { ...tweet, id: tweet._id.toString() } : tweet;
+};
